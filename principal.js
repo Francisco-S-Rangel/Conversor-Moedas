71b2559fdd37ebf1e68f1a56c0250f336379540c
Francisco Souza Rangel <83520199+Francisco-S-Rangel@users.noreply.github.com>
2021-08-29 18:17:15 -0300

    Corrigindo função formatData()
    A função formataData() não estava funcionando da maneira adequada, devido a um pequeno erro de código.

--- principal.js
+++ principal.js
@@ -287,9 +287,9 @@ function populateCurrenciesList() {
         </li>`);
   }
   
-  //função que formta a data atual 
+  //função que formata a data atual 
   function formataData(data = new Date()){
-      var dia = data.getDay();
+      var dia = data.getDate();
       var mes = data.getMonth()+1;
       var ano = data.getFullYear();
       if(dia.toString().length ==1) dia = '0' + dia;
